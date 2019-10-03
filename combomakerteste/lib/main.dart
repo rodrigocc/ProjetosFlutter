71daa228e6af50111b98f1d17151d8f8ca030aad
Rodrigo <rodrigoasilvacc@gmail.com>
2019-10-03 14:38:23 -0300

    Projeto ComboMaker

---
+++ combomakerteste/lib/main.dart
@@ -0,0 +1,69 @@
+//RodrigoAsi_
+
+import 'package:flutter/material.dart';
+import 'menu_startpage.dart'; // Importando a File Criada para a classe Menu
+import 'dart:async';
+
+
+//////////////////////////////////////MAIN////////////////////////////////////////////////
+
+
+void main(){
+
+
+  runApp(MaterialApp(
+    home: SplashPage(),
+
+  ),
+  );
+  
+}
+
+///////////////////////////////////CLASSE DA SPLASH SCREEN ///// /////////////////////////////////////
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState() {
+
+    super.initState();
+    Future.delayed(Duration(
+      seconds: 3,
+    ),(){
+      Navigator.push(context, MaterialPageRoute(
+        builder: (context)=>MenuStartPage(),
+      ));
+
+    }
+    );
+  }
+
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("Images/Framesplash.png",fit:BoxFit.fill,height: 200.0,),
+            Text("Street Workout Combos",style: TextStyle(
+              fontStyle: FontStyle.italic , fontSize: 30.0 ,fontWeight: FontWeight.bold,
+            ),),
+          ],
+        ),
+      ),
+
+    );
+  }
+}
+
+
+
+
---
+++ combomakerteste/lib/menu_startpage.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+
+////////////////////////////////////////////////
+
+class MenuStartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:Colors.lightGreen,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Página Menu Teste"),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
