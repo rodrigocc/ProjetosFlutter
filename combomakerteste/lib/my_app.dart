c2f1e553ee4399af6da0b97400ea64aadb85ca89
Philipe Soares <philipesoares@cc.ci.ufpb.br>
2019-12-28 15:30:52 -0300

    feat MyApp file in project

---
+++ combomakerteste/lib/my_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'src/splash_screen.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
